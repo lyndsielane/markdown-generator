b35f4a124ecae8896e3c8e91d22e7479d7330378
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-08 17:08:57 -0400

    question array and packages

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
-
+const fs = require(fs);
+const inquirer = require(inquirer);
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What is your username?",
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: "What is your email?",
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: "What is the name  of your project?",
+        name: 'project'
+    },
+    {
+        type: 'input',
+        message: "Please provide a short description of your project.",
+        name: 'description'
+    },
+    {
+        type: 'checkbox',
+        message: 'Please choose your license type.',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: "Name a command to install.",
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: "Name a command to test.",
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: "Advise user information for using.",
+        name: 'using'
+    },
+    {
+        type: 'input',
+        message: "Advise user information for contributing.",
+        name: 'contribution'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
